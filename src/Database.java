df35b9e28888101adb007c89299dc2556c63fdd8
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2019-03-12 13:45:42 +0000

    Did not import decimal format

--- src/Database.java
+++ src/Database.java
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.text.DecimalFormat;
 /**
 * Allows the user to interact with the database
 */
